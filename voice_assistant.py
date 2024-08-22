8ec0db4935adb7f98286bfe5b33743b586a5950a
Jiten Rai <raijiten96@gmail.com>
2024-08-22 21:22:38 +0545

    jarvis ai

--- voice_assistant.py
+++ voice_assistant.py
@@ -3,6 +3,8 @@ import speech_recognition as sr
 import webbrowser
 import datetime
 import pyjokes
+import os
+import time
 
 def sptext():
     recognizer = sr.Recognizer()
@@ -27,13 +29,42 @@ def speechtx(x):
     engine.runAndWait()
 
 if __name__ == '__main__':
-    # if sptext().lower() == 'jarvis':
-        data1 = sptext().lower()
-        if 'your name' in data1:
-            name = "My name is Javis."
-            speechtx(name)
-        elif 'old are you' in data1:
-             age = "I have just been created!"
-             speechtx(age)
-    # else:
-        # print("Call me by name Jarvis to get tasks executed!")
+    if 'jarvis' in sptext().lower(): 
+        while True:
+                data1 = sptext().lower()
+
+                if "how are you" in data1:
+                    reply = 'I am doing great, How are you?'
+                    speechtx(reply)
+                if 'your name' in data1:
+                    name = "My name is Javis."
+                    speechtx(name)
+                elif 'old are you' in data1:
+                    age = "I have just been created!"
+                    speechtx(age)
+                elif "time now" in data1:
+                    time = datetime.datetime.now().strftime("%I%M%p") #%I gives hour, %M gives minute, %p gives am pm
+                    speechtx(time)
+                elif 'youtube' in data1:
+                    webbrowser.open('https://www.youtube.com/')
+                elif 'chat gpt' in data1:
+                    webbrowser.open('https://chatgpt.com/')
+                elif 'bargaining' in data1:
+                    webbrowser.open('https://www.youtube.com/watch?v=X_e5z_XrlzY&list=PLe9t8KT-SdWXaS4thPTG66mazDoJ2-BJq&index=3')
+                elif 'joke' in data1:
+                    joke = pyjokes.get_joke(language='en', category='all')
+                    print(joke)
+                    speechtx(joke)
+                elif 'music' in data1:
+                    add = r'C:\Users\ACER\OneDrive\Desktop\New folder'
+                    list_of_song = os.listdir(add)
+                    print(list_of_song)
+                    os.startfile(os.path.join(add, list_of_song[0]))
+                elif "exit" in data1:
+                    speechtx("I am always at your service. Come again.")
+                    break    
+                # time.sleep(5)
+    else:
+        print("Call by my name Jarvis for service.")
+        sptext("activate by calling me by my name Jarvis.")
+   
